e8af67293db9e7d607402cec135312e7a92c7e7b
miffa <greatmiffa@gmail.com>
2021-07-14 15:46:08 +0800

    Create figlet_tools.sh

---
+++ my_vim/figlet_tools.sh
@@ -0,0 +1,4 @@
+wget https://download-ib01.fedoraproject.org/pub/epel/7/x86_64/Packages/f/figlet-2.2.5-9.el7.x86_64.rpm
+rpm -i figlet-2.2.5-9.el7.x86_64.rpm
+showfigfonts
+figlet -f  xxxx   "message"
